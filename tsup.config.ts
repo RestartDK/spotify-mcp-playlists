2754ec7ec0909b3d08d4578837d8395690195857
R3call <thenoobyzombie@gmail.com>
2025-04-14 16:17:17 +0200

    build: new build of tsup with explicit esnext target

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,15 +3,13 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts', 'src/bin-auth.ts'],
   format: ['esm'],
-  splitting: false,
-  sourcemap: true,
+  target: 'esnext',
+  outDir: 'dist',
+  outExtension: ({ format }) => ({
+    js: '.js',
+  }),
+
   clean: true,
   dts: true,
-  outDir: 'dist',
   shims: true,
-  target: 'node18',
-  external: [
-    '@modelcontextprotocol/sdk',
-    '@spotify/web-api-ts-sdk'
-  ]
 }); 
